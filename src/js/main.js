409c203690cd4b052a73ba4d08e5df6673035764
bcheban <128683111+bcheban@users.noreply.github.com>
2025-02-26 17:42:46 +0200

    added styles for cards

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// template for adaptive - strart
+
+const main = document.querySelector('.main')
+
+for (let index = 0; index < 20; index++) {
+    main.innerHTML += `
+    <div class="card">
+                <img src="/src/images/Rectangle 6.png" alt="" class="card-img">
+                <div class="card-deco"
+                <h2 class="card-name">Eurovision 2021 finals!</h2>
+                <h3 class="card-date">2021-05-13</h3>
+                <p class="card-location__name">Palace of Ukraine</p>
+    </div>
+    `
+    
+}
+
+// template for adaptive - strart
