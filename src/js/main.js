e08a6ba4c9c2bd214bd6b1106c8d98f3d7c8a4fa
bcheban <128683111+bcheban@users.noreply.github.com>
2025-03-01 11:59:44 +0200

    finished adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ for (let index = 0; index < 20; index++) {
     main.innerHTML += `
     <div class="card">
                 <img src="/src/images/Rectangle 6.png" alt="" class="card-img">
-                <div class="card-deco"
+                <div class="card-deco">
                 <h2 class="card-name">Eurovision 2021 finals!</h2>
                 <h3 class="card-date">2021-05-13</h3>
                 <p class="card-location__name">Palace of Ukraine</p>
