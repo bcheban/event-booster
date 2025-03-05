9c554ac847465c5c22be36074c521f76caedf170
bcheban <128683111+bcheban@users.noreply.github.com>
2025-03-05 06:09:18 +0200

    did a script for modal, but in needs a fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// template for adaptive - strart
+// template for adaptive - start
 
 const main = document.querySelector('.main')
 
@@ -15,4 +15,38 @@ for (let index = 0; index < 20; index++) {
     
 }
 
-// template for adaptive - strart
+// template for adaptive - end
+
+
+// script for modal - start 
+const cards = document.querySelectorAll('.card');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const modalClose = document.querySelector(".modal-close");
+
+function closeModal() {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
+
+function openModal() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+}
+
+// Close modal when clicking on close button
+document.querySelector(".modal-close").addEventListener("click", closeModal);
+
+// Close modal when clicking outside of it
+overlay.addEventListener("click", function (event) {
+    if (event.target === overlay) {
+        closeModal();
+    }
+});
+
+// Open modal when clicking on any card
+cards.forEach((card) => {
+    card.addEventListener("click", openModal);
+});
+
+// script for modal - end 
