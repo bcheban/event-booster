f847de9a31b73e698ee19c5a784fdb9394273614
bcheban <128683111+bcheban@users.noreply.github.com>
2025-03-08 14:06:52 +0200

    did the prevoius action again

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ const main = document.querySelector('.main')
 for (let index = 0; index < 20; index++) {
     main.innerHTML += `
     <div class="card">
-                <img src="/src/images/Rectangle 6.png" alt="" class="card-img">
+                <img src="images/Rectangle 6.png" alt="" class="card-img">
                 <div class="card-deco">
                 <h2 class="card-name">Eurovision 2021 finals!</h2>
                 <h3 class="card-date">2021-05-13</h3>
